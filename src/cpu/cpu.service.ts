b2ad24439682c26e1f66279e83c8b2e5e6c44581
josuembuyu6 <josuembuyu94@gmail.com>
2022-07-17 19:02:07 +0100

    Organizing Code with Modules

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  constructor(private poweService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from power Service');
+    this.poweService.supplyPower(10);
+    return a + b;
+  }
+}
